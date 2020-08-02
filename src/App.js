ac8800cbbe6d80dfc049df30fd630ef13af50079
mHamzaDanish <64928324+mHamzaDanish@users.noreply.github.com>
2020-08-02 21:59:50 +0500

    Update App.js
    update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>hello world</h1>
+       <h1>React Practice App</h1>
       </header>
     </div>
   );
