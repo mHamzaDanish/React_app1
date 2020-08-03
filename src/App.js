8beed99915e8cb9958fa478cf7c5c5f40083720a
mHamzaDanish <64928324+mHamzaDanish@users.noreply.github.com>
2020-08-03 10:48:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>React Practice App</h1>
-      </header>
-    </div>
+<div>hello world</div>
   );
 }
 
